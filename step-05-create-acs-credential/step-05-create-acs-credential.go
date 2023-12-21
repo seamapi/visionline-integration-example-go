567b5c85e740bc93607681e56e56a37652a3ea97
Lucas Neiva <lucasneiva07@gmail.com>
2023-12-21 19:30:07 -0300

    add step 04 create acs user and step 05 create acs credential

---
+++ step-05-create-acs-credential/step-05-create-acs-credential.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	api "github.com/seamapi/go"
+	"github.com/seamapi/go/acs"
+	goclient "github.com/seamapi/go/client"
+)
+
+func main() {
+
+	client := goclient.NewClient(goclient.WithApiKey("seam_test2US6_9G4L2sJPeso5pitYJFa2Jpto"))
+
+	systems, sErr := client.Acs.Systems.List(context.Background(), nil)
+
+	if sErr != nil {
+		log.Panic(sErr)
+	}
+
+	var visionlineSystem *api.AcsSystem
+
+	for _, s := range systems.AcsSystems {
+		if s.ExternalType == "visionline_system" {
+			visionlineSystem = s
+		}
+	}
+
+	users, err := client.Acs.Users.List(context.Background(), &acs.UsersListRequest{
+		AcsSystemId: visionlineSystem.AcsSystemId,
+	})
+
+	isMultiPhoneSyncCredential := true
+
+	log.Println(users, err)
+
+	user, uErr := client.Acs.Credentials.Create(context.Background(), &acs.CredentialsCreateRequest{
+		AcsUserId:                  users.AcsUsers[0].AcsUserId,
+		AccessMethod:               "mobile_key",
+		IsMultiPhoneSyncCredential: &isMultiPhoneSyncCredential,
+	})
+
+	if uErr != nil {
+		log.Panic(uErr)
+	}
+
+	log.Println(user)
+}
