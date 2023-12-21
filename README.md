120ebc698ce8cf0e9b8805d217db52045b01e8b2
Lucas Neiva <lucasneiva07@gmail.com>
2023-12-21 18:00:11 -0300

    readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Visionline Integration Example Go
 
+To run the steps you just need have Go 1.21.4 or later installed and run `go run .` inside of any step folder.
--- main.go
+++ main.go
@@ -1,29 +1,11 @@
 package main
 
 import (
-	"context"
 	"log"
-
-	api "github.com/seamapi/go"
-	goclient "github.com/seamapi/go/client"
 )
 
 func main() {
 
 	log.Println("Visionline integration example init")
-
-	client := goclient.NewClient(goclient.WithApiKey("seam_test2US6_9G4L2sJPeso5pitYJFa2Jpto"))
-
-	webview, wvErr := client.ConnectWebviews.Create(context.Background(), &api.ConnectWebviewsCreateRequest{
-		AcceptedProviders: []api.AcceptedProvider{"visionline"},
-	})
-
-	if wvErr != nil {
-		log.Panic(wvErr)
-	}
-
-	log.Println(webview)
-
-	// open webview.Url and connect the account through webview
 
 }
